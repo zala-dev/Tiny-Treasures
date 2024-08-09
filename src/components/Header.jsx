b634bf22c5f39bd3145f8401e6b7fab2a259f687
Zala <zala.developer@gmail.com>
2024-08-09 21:02:47 +0930

    complete navbar and product list page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaCartShopping, FaUser } from "react-icons/fa6";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand>Tiny Treasures</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link>
+                <FaCartShopping /> Cart
+              </Nav.Link>
+              <Nav.Link>
+                <FaUser /> Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 py-3 rounded" title={product.name}>
+      <Card.Img src={product.image} className="card-img-top" />
+      <Card.Body>
+        <Card.Title as="div" className="product-title">
+          {product.name}
+        </Card.Title>
+        <Card.Text as="div">{`${product.numReviews} reviews`}</Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Product;
