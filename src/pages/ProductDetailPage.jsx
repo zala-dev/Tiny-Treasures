de0928339d143ced50a43322430c4aa3ae15556c
Zala <zala.developer@gmail.com>
2024-08-10 20:59:14 +0930

    refactor removed handleclick func

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import products from "../mock-data/productsMockData.js";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 const ProductDetailPage = () => {
   const { id: productId } = useParams();
-  const navigate = useNavigate();
 
   const product = products.find((product) => product.id === Number(productId));
 
@@ -12,13 +11,9 @@ const ProductDetailPage = () => {
     return <div>Product not found</div>;
   }
 
-  const handleBackClick = () => {
-    navigate(-1);
-  };
-
   return (
     <div>
-      <button onClick={handleBackClick}>Back</button>
+      <Link to="/">Go Back</Link>
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p>Price: ${product.price}</p>
