7b06b5276fc9c42c331c4d67fd590f7a12160293
Zala <zala.developer@gmail.com>
2024-08-10 14:37:55 +0930

    add Footer component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
-import { products } from "../mock-data/productsMockData.js";
+import products from "../mock-data/productsMockData.js";
 
 const HomePage = () => {
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
-        {products.map((product) => (
+        {products?.map((product) => (
           <Col key={product.name} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
