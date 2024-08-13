da2e073a787717047ce77567be44dea0295c7bb3
Zala <zala.developer@gmail.com>
2024-08-13 20:25:11 +0930

    update func name

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,10 +1,10 @@
-const notFound = (req, res, next) => {
+const notFoundMiddleware = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
-const errorHandler = (err, req, res, next) => {
+const errorHandlerMiddleware = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
@@ -14,4 +14,4 @@ const errorHandler = (err, req, res, next) => {
   });
 };
 
-export { errorHandler, notFound };
+export { errorHandlerMiddleware, notFoundMiddleware };
