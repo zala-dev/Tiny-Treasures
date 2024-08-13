8fd3cacb6e503b5bfb943e0dbbb8b23ad5d7aca7
Zala <zala.developer@gmail.com>
2024-08-13 20:09:07 +0930

    refactor middlewares to maintain consistency of using modules instead of common js

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,18 +1,12 @@
-const notFoundMiddleware = (req, res, next) => {
+const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
-const errorHandlerMiddleware = (err, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
-
-  // check mongoose bad ObjectId error
-  if (err.name === "CastError" && err.kind === "ObjectId") {
-    message = "Resource not found";
-    statusCode = 404;
-  }
 
   res.status(statusCode).json({
     message,
@@ -20,7 +14,4 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   });
 };
 
-module.exports = {
-  errorHandlerMiddleware,
-  notFoundMiddleware,
-};
+export { errorHandler, notFound };
