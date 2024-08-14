a4403927be4a00e072d1ae70bba09f9d577ae825
Zala <zala.developer@gmail.com>
2024-08-14 23:08:10 +0930

    update product detail page and add rating component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 py-3 rounded" title={product.name}>
       <Card.Img src={product.image} className="card-img-top" />
       <Card.Body>
-        <Link to={`product/${product.id}`}>
+        <Link to={`product/${product._id}`}>
           <Card.Title as="div" className="product-title">
             {product.name}
           </Card.Title>
         </Link>
-        <Card.Text as="div">{`${product.numReviews} reviews`}</Card.Text>
-        <Card.Text as="h3">${product.price}</Card.Text>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+        <Card.Text as="h3">AU${product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,23 +1,117 @@
 import React from "react";
-import products from "../mock-data/productsMockData.js";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+  Form,
+} from "react-bootstrap";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import Rating from "../components/Rating";
+
 import { useParams, Link } from "react-router-dom";
+import { useGetProductDetailsQuery } from "../slices/productApiSlice";
 
 const ProductDetailPage = () => {
   const { id: productId } = useParams();
 
-  const product = products.find((product) => product.id === Number(productId));
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useGetProductDetailsQuery(productId);
 
   if (!product) {
     return <div>Product not found</div>;
   }
 
   return (
-    <div>
-      <Link to="/">Go Back</Link>
-      <h1>{product.name}</h1>
-      <p>{product.description}</p>
-      <p>Price: ${product.price}</p>
-    </div>
+    <>
+      <Link to="/" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <Row>
+          <Col md={5}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={4}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+              <ListGroupItem>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
+              </ListGroupItem>
+              <ListGroupItem>Price: AU${product.price}</ListGroupItem>
+              <ListGroupItem>Description: {product.description}</ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price: </Col>
+                    <Col>
+                      <strong>AU${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status: </Col>
+                    <Col>
+                      <strong>
+                        {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                      </strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col>
+                        <Form.Control as="select">
+                          {[...Array(product.countInStock).keys()].map((el) => (
+                            <option key={el + 1} value={el + 1}>
+                              {el + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+                <ListGroupItem>
+                  <Button
+                    className="btn-block"
+                    type="button"
+                    disabled={product.countInStock === 0}
+                  >
+                    Add To Cart
+                  </Button>
+                </ListGroupItem>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
+    </>
   );
 };
 
