3ea66e693c68be42c75cf0eab051af23abc95196
Zala <zala.developer@gmail.com>
2024-08-16 23:01:44 +0930

    ready to deploy on render

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,18 @@ app.use("/api/upload", uploadRoutes);
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Welcome to Tiny Treasures");
+  });
+}
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
