5fede593591f060e15aecd50b9df9ca467de376f
Zala <zala.developer@gmail.com>
2024-08-17 13:27:39 +0930

    fix - create product default image path

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -43,7 +43,7 @@ const createProduct = asyncHandler(async (req, res) => {
     name: "Sample name",
     price: 0,
     user: req.user._id,
-    image: "/public/images/sample.jpg",
+    image: "frontend/build/images/sample.jpg",
     brand: "Sample brand",
     category: "Sample category",
     countInStock: 0,
